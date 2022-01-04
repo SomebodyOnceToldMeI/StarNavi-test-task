1df90ef5eff80ba088d70c57183e766c851bec55
Yury <SomebodyOnceToldMeI>
2022-01-04 12:32:30 +0200

    Add create_post_request_processor and more messages

---
+++ request_processors/create_post_request_processor.py
@@ -0,0 +1,22 @@
+from request_processor import RequestProcessor
+from error_messages import *
+
+class CreatePostRequestProcessor(RequestProcessor):
+    required_fields = ['access_token', 'post_text']
+
+    def process(self):
+        payload = self.request.json
+        error_message = self._verify_payload(payload)
+        if error_message:
+            return error_message
+
+        user = self.database.get_user_by_access_token(payload['access_token'])
+        if not user:
+            return INCORRECT_ACCESS_TOKEN_MESSAGE
+
+        post = self.database.create_post(user, payload['post_text'])
+
+        message = SUCCESSFULL_POST_CREATE_MESSAGE.copy()
+        message['post_id'] = post.get_id()
+
+        return message
--- request_processors/error_messages.py
+++ request_processors/error_messages.py
@@ -5,3 +5,7 @@ SUCCESSFULL_SIGNUP_MESSAGE = {'status_code' : 200, 'message' : 'You are successf
 USER_DOES_NOT_EXIST_ERROR_MESSAGE = {'status_code' : 404, 'message' : 'User does not exist.'}
 
 SUCCESSFULL_LOGIN_MESSAGE = {'status_code' : 200, 'message' : 'You are successfully logged in.', 'access_token' : None}
+
+INCORRECT_ACCESS_TOKEN_MESSAGE = {'status_code' : 401, 'message' : 'Provided access token is incorrect.'}
+
+SUCCESSFULL_POST_CREATE_MESSAGE = {'status_code' : 200, 'message' : 'Post was successfully created.', 'post_id' : None}
