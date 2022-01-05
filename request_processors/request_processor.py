0028d1e72326f1ada69763dbdaf94dd773f0dd66
Yury <SomebodyOnceToldMeI>
2022-01-05 12:12:24 +0200

    Fix bugs in request_processor (missing self. and import)

--- request_processors/request_processor.py
+++ request_processors/request_processor.py
@@ -1,3 +1,5 @@
+from .error_messages import BAD_REQUEST_ERROR_MESSAGE
+
 class RequestProcessor:
     required_input_keys = [] #keys that request payload has to contain
 
@@ -12,12 +14,10 @@ class RequestProcessor:
         if not payload:
             return BAD_REQUEST_ERROR_MESSAGE
 
-        for key in required_input_keys:
+        for key in self.required_input_keys:
             if not key in payload.keys():
                 return BAD_REQUEST_ERROR_MESSAGE
 
-        for key in required_input_keys:
+        for key in self.required_input_keys:
             if not payload[key]:
                 return BAD_REQUEST_ERROR_MESSAGE
-
-    
