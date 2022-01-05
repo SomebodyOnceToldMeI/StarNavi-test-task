cf79ed071bf1f9171ac13040c5da1b8c722aa690
Yury <SomebodyOnceToldMeI>
2022-01-05 12:29:32 +0200

    Fix typo and move success messages into try block

--- tests/signup_request_tests.py
+++ tests/signup_request_tests.py
@@ -26,31 +26,32 @@ def user_gets_error_if_payload_is_incorrect():
     message = rp.process()
     try:
         assert message == BAD_REQUEST_ERROR_MESSAGE
+        print('Test passed.')
     except:
         print('Test failed.')
         print('Returned message: ', message)
-    print('Test passed.')
+
 
 def user_gets_error_if_is_already_registered_and_user_is_not_created_in_database():
-    print('Running test: user_gets_error_if_is_already_registered')
+    print('Running test: user_gets_error_if_is_already_registered_and_user_is_not_created_in_database')
 
     payload = {'login' : 'test_login', 'password' : 'test_password'}
     request = MagicMock()
     request.json = payload
 
     database = MagicMock()
-    database.get_user = MagickMock(return_value = True) #get_user returns True to imitate that user exists
+    database.get_user = MagicMock(return_value = True) #get_user returns True to imitate that user exists
 
     rp = SignupRequestProcessor(request, database)
     message = rp.process()
     try:
         assert message == USER_IS_ALREADY_SIGNED_UP
         database.create_user.assert_not_called()
-
-        
+        print('Test passed.')
     except:
         print('Test failed.')
         print('Returned message: ', message)
-    print('Test passed.')
+
 
 user_gets_error_if_payload_is_incorrect()
+user_gets_error_if_is_already_registered_and_user_is_not_created_in_database()
