81d34d74ddaf0fd1a1a8dc3bd8e713827f46f759
Yury <SomebodyOnceToldMeI>
2022-01-05 12:23:33 +0200

    Add user_gets_error_if_is_already_registered test

--- tests/signup_request_tests.py
+++ tests/signup_request_tests.py
@@ -31,4 +31,26 @@ def user_gets_error_if_payload_is_incorrect():
         print('Returned message: ', message)
     print('Test passed.')
 
+def user_gets_error_if_is_already_registered_and_user_is_not_created_in_database():
+    print('Running test: user_gets_error_if_is_already_registered')
+
+    payload = {'login' : 'test_login', 'password' : 'test_password'}
+    request = MagicMock()
+    request.json = payload
+
+    database = MagicMock()
+    database.get_user = MagickMock(return_value = True) #get_user returns True to imitate that user exists
+
+    rp = SignupRequestProcessor(request, database)
+    message = rp.process()
+    try:
+        assert message == USER_IS_ALREADY_SIGNED_UP
+        database.create_user.assert_not_called()
+
+        
+    except:
+        print('Test failed.')
+        print('Returned message: ', message)
+    print('Test passed.')
+
 user_gets_error_if_payload_is_incorrect()
