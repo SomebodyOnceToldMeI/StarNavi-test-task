65ed302a73b4e5c5ec58be14caa80ef74987a11b
Yury <SomebodyOnceToldMeI>
2022-01-06 12:42:16 +0200

    Add run_all_tests.py script to run all tests

---
+++ tests/run_all_tests.py
@@ -0,0 +1,11 @@
+
+print('**************\nRunning signup request tests:')
+import signup_request_tests
+print('**************\nRunning login request tests:')
+import login_request_tests
+print('**************\nRunning create post request tests:')
+import create_post_request_tests
+print('**************\nRunning like post request tests:')
+import like_post_request_tests
+print('**************\nRunning unlike post request tests:')
+import unlike_post_request_tests
